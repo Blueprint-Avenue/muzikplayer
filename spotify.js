7e620a8c62bc9cdb8806acdbcbdf513b46462cab
Samiya <skmcintosh20@gmail.com>
2022-04-01 11:48:55 -0700

    added spotify api

---
+++ spotify.js
@@ -0,0 +1,8 @@
+const authEndpoint = "https://accounts.spotify.com/authorize?";
+const clientId = "2a32a4ad3fde414788615dbd7c90a1ba";
+const redirectUri = "https://localhost:3000";
+const scopes = ["user-library-read", "playlist-read-private"];
+
+export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+	"%20"
+)}&response_type=token&show_dialog=true`;
